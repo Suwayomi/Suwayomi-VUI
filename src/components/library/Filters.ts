e40c659462914724a911494d4d7285e3ad13e6ae
Robonau <30987265+Robonau@users.noreply.github.com>
2022-10-26 18:48:15 +0000

    fixed filters being null instead of  'null'

--- src/components/library/Filters.ts
+++ src/components/library/Filters.ts
@@ -1,43 +1,47 @@
 import { ref } from 'vue';
 import { LocalStorage } from 'quasar';
 
-const unread = ref(<boolean | null>LocalStorage.getItem('lbUnread'));
-const downloaded = ref(<boolean | null>LocalStorage.getItem('lbDownloaded'));
-const leftToRead = ref(<boolean | null>LocalStorage.getItem('lbLeftToRead'));
-const alphabetical = ref(
-  <boolean | null>LocalStorage.getItem('lbAlphabetical')
+const unread = ref(<boolean | null | 'null'>LocalStorage.getItem('lbUnread'));
+const downloaded = ref(
+  <boolean | null | 'null'>LocalStorage.getItem('lbDownloaded')
 );
-const ByID = ref(<boolean | null>LocalStorage.getItem('lbByid'));
+const leftToRead = ref(
+  <boolean | null | 'null'>LocalStorage.getItem('lbLeftToRead')
+);
+const alphabetical = ref(
+  <boolean | null | 'null'>LocalStorage.getItem('lbAlphabetical')
+);
+const ByID = ref(<boolean | null | 'null'>LocalStorage.getItem('lbByid'));
 
 export default function useInBar() {
-  const setUnread = (data: boolean | null) => {
+  const setUnread = (data: boolean | null | 'null') => {
     LocalStorage.set('lbUnread', data);
     unread.value = data;
   };
-  const setDownloaded = (data: boolean | null) => {
+  const setDownloaded = (data: boolean | null | 'null') => {
     LocalStorage.set('lbDownloaded', data);
     downloaded.value = data;
   };
-  const setLeftToRead = (data: boolean | null) => {
-    if (data != null) {
+  const setLeftToRead = (data: boolean | null | 'null') => {
+    if (data != 'null') {
       LocalStorage.set('lbLeftToRead', data);
-      LocalStorage.set('lbAlphabetical', null);
-      LocalStorage.set('lbByid', null);
+      LocalStorage.set('lbAlphabetical', 'null');
+      LocalStorage.set('lbByid', 'null');
     }
     leftToRead.value = data;
   };
-  const setAlphabetical = (data: boolean | null) => {
-    if (data != null) {
-      LocalStorage.set('lbLeftToRead', null);
+  const setAlphabetical = (data: boolean | null | 'null') => {
+    if (data != 'null') {
+      LocalStorage.set('lbLeftToRead', 'null');
       LocalStorage.set('lbAlphabetical', data);
-      LocalStorage.set('lbByid', null);
+      LocalStorage.set('lbByid', 'null');
     }
     alphabetical.value = data;
   };
-  const setByID = (data: boolean | null) => {
-    if (data != null) {
-      LocalStorage.set('lbLeftToRead', null);
-      LocalStorage.set('lbAlphabetical', null);
+  const setByID = (data: boolean | null | 'null') => {
+    if (data != 'null') {
+      LocalStorage.set('lbLeftToRead', 'null');
+      LocalStorage.set('lbAlphabetical', 'null');
       LocalStorage.set('lbByid', data);
     }
     ByID.value = data;
