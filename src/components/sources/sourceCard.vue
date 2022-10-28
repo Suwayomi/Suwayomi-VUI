107a422dc51cef9ff88408c7b170134ed846cf97
Robonau <30987265+Robonau@users.noreply.github.com>
2022-10-28 22:48:02 +0000

    sources page

---
+++ src/components/sources/sourceCard.vue
@@ -0,0 +1,102 @@
+<template>
+  <q-card v-ripple flat class="q-ma-sm">
+    <q-item style="height: 100px">
+      <div class="row content-center col-grow">
+        <q-item-section avatar>
+          <q-img
+            :src="imgdata"
+            loading="lazy"
+            spinner-color="white"
+            style="height: 100px; aspect-ratio: 225/225; width: fit-content"
+            class="rounded-borders items-center justify-center"
+            no-spinner
+          >
+            <q-inner-loading
+              :showing="!imgdata"
+              color="primary"
+              class="bg-transparent"
+            >
+            </q-inner-loading>
+          </q-img>
+        </q-item-section>
+        <q-item-section class="flex-grow">
+          <q-item-label>{{ capitalizeFirstLetter(source.name) }}</q-item-label>
+          <q-item-label caption
+            >{{ langCodeToName(source.lang) }}
+            <span v-if="source.isNsfw" style="color: red">18+</span>
+          </q-item-label>
+        </q-item-section>
+      </div>
+      <div class="flex items-center">
+        <q-btn
+          class="q-ma-sm"
+          outline
+          color="blue"
+          label="latest"
+          :to="`/sources/` + source.id + `/latest/`"
+        >
+        </q-btn>
+        <q-btn
+          class="q-ma-sm"
+          outline
+          color="blue"
+          label="browse"
+          :to="`/sources/` + source.id + `/popular/`"
+        >
+        </q-btn>
+      </div>
+    </q-item>
+  </q-card>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType, ref } from 'vue';
+import { useQuasar } from 'quasar';
+import fetcher from '../global/fetcher';
+import { source } from '../global/models';
+import { langCodeToName } from 'src/components/extantions/language';
+
+export default defineComponent({
+  name: 'sourceCard',
+  props: {
+    source: {
+      type: Object as PropType<source>,
+      required: true
+    }
+  },
+  emits: ['reload'],
+  methods: {
+    capitalizeFirstLetter(string: string): string {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    },
+    langCodeToName,
+    getSetImg() {
+      fetcher(this.source.iconUrl + '?useCache=' + this.useCache)
+        .then((resp) => resp.blob())
+        .then((blob) => {
+          let reader = new FileReader();
+          reader.readAsDataURL(blob);
+          reader.onloadend = () => {
+            var base64data = reader.result as string;
+            this.imgdata = base64data;
+          };
+        });
+    }
+  },
+  created: function () {
+    this.getSetImg();
+  },
+  watch: {
+    'source.iconUrl'() {
+      this.imgdata = '';
+      this.getSetImg();
+    }
+  },
+  setup() {
+    const $q = useQuasar();
+    const useCache = ref(`${$q.localStorage.getItem('useCache')}`);
+    const imgdata = ref('');
+    return { useCache, imgdata };
+  }
+});
+</script>
--- src/pages/sourcesPage.vue
+++ src/pages/sourcesPage.vue
@@ -1,5 +1,23 @@
 <template>
   <q-page>
+    <q-list>
+      <div v-for="(source, index) in uselist" :key="index">
+        <div
+          v-if="
+            is.deepEqual(
+              uselist.find((ele) => ele.lang == source.lang),
+              source
+            )
+          "
+          class="text-h4 q-ml-xl q-my-lg"
+        >
+          {{ langCodeToName(source.lang) }}
+        </div>
+        <q-intersection style="height: 100px; width: 100%" class="flex-shrink">
+          <sourcecard @reload="reload" :source="source"></sourcecard>
+        </q-intersection>
+      </div>
+    </q-list>
     <q-inner-loading
       style="
         position: fixed;
@@ -10,7 +28,7 @@
         height: fit-content;
         background-color: transparent;
       "
-      :showing="true"
+      :showing="!uselist.length"
       color="primary"
     >
     </q-inner-loading>
@@ -18,13 +36,56 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import fetcher from 'src/components/global/fetcher';
+import useInBar from 'src/components/global/inBar';
+import { source } from 'src/components/global/models';
+import { defineComponent, ref } from 'vue';
+import SearchBar from 'src/components/extantions/TopBar.vue';
+import Filters from 'src/components/extantions/Filters';
+import sourcecard from 'src/components/sources/sourceCard.vue';
+import { is } from 'quasar';
+import { langCodeToName } from 'src/components/extantions/language';
 
 export default defineComponent({
   name: 'sourcesPage',
+  components: { sourcecard },
+  created: function () {
+    const Bar = useInBar();
+    Bar.setInBar(SearchBar);
+    this.reload();
+  },
+  methods: {
+    reload() {
+      fetcher('/api/v1/source/list')
+        .then((resp) => resp.json())
+        .then((data: source[]) => {
+          this.list = data;
+          this.filters.setcurrlangs(this.extractLangs(data));
+        });
+    },
+    langCodeToName,
+    extractLangs(lis: source[]): string[] {
+      return Array.from(new Set(lis.map((ele) => ele.lang))).filter((elee) => {
+        if ('all' == elee) {
+          return false;
+        }
+        return true;
+      });
+    }
+  },
+  computed: {
+    uselist(): source[] {
+      return this.list.filter((ele) => {
+        return [...this.langs, 'all'].includes(ele.lang);
+      });
+    }
+  },
   setup(_props, { emit }) {
     emit('setTitle', 'Sources');
-    return {};
+    const filters = Filters();
+    const langs = ref(filters.langs);
+    const list = ref(<source[]>[]);
+    return { list, langs, filters, is };
   }
 });
 </script>
