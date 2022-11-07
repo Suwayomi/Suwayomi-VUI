542b4268a1b02bd7715636f1b177f970462b203b
Robonau <30987265+Robonau@users.noreply.github.com>
2022-11-07 00:15:29 +0000

    adding manga to catagories

---
+++ src/components/manga/TopBcat.vue
@@ -0,0 +1,35 @@
+<template>
+  <q-item>
+    <q-toggle v-model="incat" :label="cat.name"></q-toggle>
+  </q-item>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType, ref } from 'vue';
+import { cat } from '../global/models';
+
+export default defineComponent({
+  props: {
+    sele: {
+      type: Boolean as PropType<boolean>,
+      required: true
+    },
+    cat: {
+      type: Object as PropType<cat>,
+      required: true
+    }
+  },
+  watch: {
+    incat() {
+      this.$fetch(
+        `/api/v1/manga/${this.$route.params['mangaID']}/category/${this.cat.id}`
+      );
+    }
+  },
+  setup(props) {
+    return {
+      incat: ref(props.sele)
+    };
+  }
+});
+</script>
