9477ed305a9985cfaf7750d84bd44d70d2be5429
Robonau <30987265+Robonau@users.noreply.github.com>
2022-11-07 00:15:29 +0000

    adding manga to catagories

---
+++ src/components/manga/topbar.vue
@@ -0,0 +1,55 @@
+<template>
+  <q-btn
+    flat
+    class="q-ml-sm"
+    round
+    :text-color="$q.dark.isActive ? `white` : `dark`"
+    icon="filter_list"
+    @click="dialo = true"
+  />
+  <q-dialog v-model="dialo">
+    <q-card>
+      <q-card-section class="q-px-xl">
+        <div class="text-h5">Set categories</div>
+      </q-card-section>
+      <q-separator></q-separator>
+      <topBcat
+        v-for="opt in options"
+        :key="opt.id"
+        :sele="curr.find((ele) => ele.id == opt.id) != undefined"
+        :cat="opt"
+      >
+      </topBcat>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import topBcat from 'src/components/manga/TopBcat.vue';
+import { cat } from '../global/models';
+
+export default defineComponent({
+  name: 'libraryTopBar',
+  components: { topBcat },
+  watch: {},
+  mounted: function () {
+    this.$fetchJSON('/api/v1/category/').then((opt: cat[]) => {
+      this.options = opt.slice(1);
+    });
+    this.$fetchJSON(
+      `/api/v1/manga/${this.$route.params['mangaID']}/category/`
+    ).then((opt: cat[]) => {
+      this.curr = opt;
+    });
+  },
+  setup() {
+    return {
+      dialo: ref(false),
+      options: ref(<cat[]>[]),
+      curr: ref(<cat[]>[]),
+      disp: ref([])
+    };
+  }
+});
+</script>
