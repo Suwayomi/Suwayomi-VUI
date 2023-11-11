73b71d0099a1d407d65521cc9d7bf2be86e14711
robonau <30987265+Robonau@users.noreply.github.com>
2023-11-11 18:24:10 +0000

    some fixes

--- src/routes/(app)/manga/[MangaID]/chapter/[ChapterID]/chapterDrawer.svelte
+++ src/routes/(app)/manga/[MangaID]/chapter/[ChapterID]/chapterDrawer.svelte
@@ -2,7 +2,7 @@
 	import IconButton from '$lib/components/IconButton.svelte';
 	import Slide from '$lib/components/Slide.svelte';
 	import { enumKeys } from '$lib/util';
-	import { getDrawerStore } from '@skeletonlabs/skeleton';
+	import { getDrawerStore, TabGroup } from '@skeletonlabs/skeleton';
 	import { ViewNav, chapterTitle, mangaTitle } from './chapterStores';
 	import { Layout, MangaMeta, Mode } from '$lib/simpleStores';
 	const drawerStore = getDrawerStore();
@@ -13,6 +13,7 @@
 	<div class="flex flex-col p-4">
 		<div class="flex justify-end border-b border-surface-500 mb-4 pb-4">
 			<IconButton
+				tabindex={999}
 				name="mdi:chevron-left"
 				height="h-16"
 				on:click={() => {
@@ -24,20 +25,17 @@
 		<h1 class="h2 line-clamp-3 my-2 pl-4">{$mangaTitle}</h1>
 		<h2 class="h3 border-y mt-4 border-surface-500 p-4">{$chapterTitle}</h2>
 		<div class="flex flex-col space-y-2 my-2">
-			<Slide class="outline-0 p-1 hover:variant-glass-surface" bind:checked={$mangaMeta.Margins}>
+			<Slide tabindex={0} class="p-1 hover:variant-glass-surface" bind:checked={$mangaMeta.Margins}>
 				Page Margins
 			</Slide>
-			<Slide class="outline-0 p-1 hover:variant-glass-surface" bind:checked={$mangaMeta.Scale}>
+			<Slide class="p-1 hover:variant-glass-surface" bind:checked={$mangaMeta.Scale}>
 				Page Scale
 			</Slide>
-			<Slide
-				class="outline-0 p-1 hover:variant-glass-surface"
-				bind:checked={$mangaMeta.SmoothScroll}
-			>
+			<Slide class="p-1 hover:variant-glass-surface" bind:checked={$mangaMeta.SmoothScroll}>
 				Smooth Scroll
 			</Slide>
 			{#if $mangaMeta.ReaderMode !== Mode.Vertical}
-				<Slide class="outline-0 p-1 hover:variant-glass-surface" bind:checked={$mangaMeta.Offset}>
+				<Slide class="p-1 hover:variant-glass-surface" bind:checked={$mangaMeta.Offset}>
 					Page Offset
 				</Slide>
 			{/if}
@@ -49,7 +47,7 @@
 					{/each}
 				</select>
 			</label>
-			<Slide class="outline-0 p-1 hover:variant-glass-surface" bind:checked={$ViewNav}>
+			<Slide class="p-1 hover:variant-glass-surface" bind:checked={$ViewNav}>
 				View Navigation Layout
 			</Slide>
 			<label class="pl-3">
