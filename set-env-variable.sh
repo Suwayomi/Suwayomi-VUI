89fa823d8e82a3572c56f0b94a120a2c57557b55
robonau <30987265+Robonau@users.noreply.github.com>
2023-11-11 00:34:29 +0000

    restart

--- set-env-variable.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-JSON_STRING='window.configs = { \
-  "TachideskURL":"'"${TachideskURL}"'", \
-  "VUE_APP_VAR2":"'"${VUE_APP_VAR2}"'" \
-}'
-sed -i "s@// CONFIGURATIONS_PLACEHOLDER@${JSON_STRING}@" /usr/share/nginx/html/index.html
-exec "$@"
