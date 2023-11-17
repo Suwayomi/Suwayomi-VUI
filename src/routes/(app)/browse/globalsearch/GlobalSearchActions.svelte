589ce9d51692e257a05514a24602165def5c3851
robonau <30987265+Robonau@users.noreply.github.com>
2023-11-17 04:10:43 +0000

    open migrating manga

--- src/routes/(app)/browse/globalsearch/GlobalSearchActions.svelte
+++ src/routes/(app)/browse/globalsearch/GlobalSearchActions.svelte
@@ -5,6 +5,9 @@
 	import { getModalStore } from '@skeletonlabs/skeleton';
 	import { langfilt } from '../BrowseStores';
 	import GlobalSearchLangfilterModal from './GlobalSearchLangfilterModal.svelte';
+	import { page } from '$app/stores';
+	import Tooltip from '$lib/components/Tooltip.svelte';
+	import IconWrapper from '$lib/components/IconWrapper.svelte';
 
 	export let rawSources: SourcesQuery['sources'] | undefined;
 	export let langs: Set<string>;
@@ -14,6 +17,16 @@
 
 <div class="h-full flex">
 	<Search />
+	{#if $page.url.pathname.includes('/browse/migrate/manga/')}
+		<Tooltip tip="open migrating manga" class="cursor-pointer h-full hover:variant-glass-surface">
+			<a
+				class="aspect-square h-full w-auto"
+				href={$page.url.pathname.replace('/browse/migrate', '')}
+			>
+				<IconWrapper name="mdi:step-backward" class="w-auto h-full p-2" />
+			</a>
+		</Tooltip>
+	{/if}
 	<TooltipIconButton
 		on:click={() => {
 			modalStore.trigger({
