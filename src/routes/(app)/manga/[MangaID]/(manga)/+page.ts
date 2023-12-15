960f535f4f08d213d712a54a2133a71a76dd0af8
robonau <30987265+Robonau@users.noreply.github.com>
2023-12-15 12:59:54 +0000

    migration to SvelteKit 2

--- src/routes/(app)/manga/[MangaID]/(manga)/+page.ts
+++ src/routes/(app)/manga/[MangaID]/(manga)/+page.ts
@@ -9,7 +9,7 @@ import type { PageLoad } from './$types';
 
 export const load: PageLoad = ({ params }) => {
 	const tmp = parseInt(params.MangaID);
-	if (isNaN(tmp)) throw error(400, 'MangaID should be a number');
+	if (isNaN(tmp)) error(400, 'MangaID should be a number');
 	return {
 		MangaID: tmp
 	};
