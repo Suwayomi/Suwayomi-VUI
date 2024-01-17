0ce8b2a9d4249330e7b6b39451f5328c64e3c81a
robonau <30987265+Robonau@users.noreply.github.com>
2024-01-17 23:14:27 +0000

    fix ipv6 resolvers

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	// @ts-expect-error Type 'Promise<Plugin<any>[]>' is not assignable to type 'PluginOption'
 	plugins: [sveltekit(), purgeCss()],
 	server: {
 		proxy: {
