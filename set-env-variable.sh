b4bc4097dcfe931d5e67c5eab1e688058525aee7
robonau <30987265+Robonau@users.noreply.github.com>
2024-01-17 22:38:29 +0000

    ipv6 resolvers fix

--- set-env-variable.sh
+++ set-env-variable.sh
@@ -8,5 +8,11 @@
 
 TMP=$(echo "$suwayomi" | sed "s@/\$@@")
 TMP2=$(awk 'BEGIN{ORS=" "} $1=="nameserver" {print $2}' /etc/resolv.conf)
-sed -i "s@resolverPLACEHOLDER@$TMP2@" /etc/nginx/conf.d/default.conf
+regex='^([0-9a-fA-F]{0,4}:){1,7}[0-9a-fA-F]{0,4}$'
+if [[ $TMP2 =~ $regex ]]; then
+  sed -i "s@resolverPLACEHOLDER@[$TMP2]@" /etc/nginx/conf.d/default.conf
+else
+  sed -i "s@resolverPLACEHOLDER@$TMP2@" /etc/nginx/conf.d/default.conf
+fi
+
 sed -i "s@PLACEHOLDER@$TMP@" /etc/nginx/conf.d/default.conf
